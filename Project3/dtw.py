d1d85348642501a002fe0f2b423d3ec07cb81ca1
davidluzhiyun <david.luzhiyun@gmail.com>
2024-02-03 07:02:57 +0800

    Fixed bug in basic dtw and tested

--- Project3/dtw.py
+++ Project3/dtw.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+import feature_extraction
 
 def euclidean_distance(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
@@ -15,17 +16,21 @@ def dtw(input, template):
     # The padded version
     p0 = np.full((n+1, m+2), np.inf)
     # The unpadded p1 = p0[1:,2:]
-    for i in range(0, n):
+    for i in range(n):
         # initialize (0,0)
         if i == 0:
             p0[1,2] = 0
             continue
         # filter out the nodes we don't go through
-        for j in range(max(0, m - 2*(n-i-1)+1), min(m, 2*i+1)):
+        for j in range(max(0, m - 2*(n-i-1)-1), min(m, 2*i+1)):
             # transform to padded coordinates
             i0 = i + 1
             j0 = j + 2
             c = euclidean_distance(input[i], template[j])
-            p0[i0, j0] = min(p0[i0 - 1, j0] + c, p0[i0 - 1, j0 - 1] + c, p0[i0, j0] + c, p0[i0-1,j0 - 2] + c)
+            p0[i0, j0] = min(p0[i0 - 1, j0] + c, p0[i0 - 1, j0 - 1] + c, p0[i0-1, j0 - 2] + c)
+    # feature_extraction.plot_spectrogram(p0[1:, 2:],name="p")
+    # print(p0[-1])
+    return p0[-1, -1]
 
-    return p0[-1,-1]
+
+# print(dtw(feature_extraction.extract_feature("three2.wav"), feature_extraction.extract_feature("three.wav")))
