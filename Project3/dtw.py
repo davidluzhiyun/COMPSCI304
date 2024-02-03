798e2eb425cbfd7155eac7a858d0d0bd26016449
davidluzhiyun <david.luzhiyun@gmail.com>
2024-02-03 15:22:28 +0800

    Fixed bug in basic dtw with initialization

--- Project3/dtw.py
+++ Project3/dtw.py
@@ -1,13 +1,15 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 import feature_extraction
+from sklearn.metrics.pairwise import euclidean_distances
 
-def euclidean_distance(x, y):
-    return np.sqrt(np.sum((x - y) ** 2))
+def dist(x,y):
+    return euclidean_distances(x.reshape(1,-1), y.reshape(1,-1))
 
 # The basic dtw routine
 # Both input and template have shape (frame count, feature count)
 # Uses Euclidean distance, allows Horizontal and Diagonal transition up to 1/2 shrinking
+# returns cost
 def dtw(input, template):
     # get the length of the input and template
     n, m = len(input), len(template)
@@ -19,18 +21,36 @@ def dtw(input, template):
     for i in range(n):
         # initialize (0,0)
         if i == 0:
-            p0[1,2] = 0
+            p0[1, 2] = dist(input[0], template[0])
             continue
         # filter out the nodes we don't go through
         for j in range(max(0, m - 2*(n-i-1)-1), min(m, 2*i+1)):
             # transform to padded coordinates
             i0 = i + 1
             j0 = j + 2
-            c = euclidean_distance(input[i], template[j])
+            c = dist(input[i], template[j])
             p0[i0, j0] = min(p0[i0 - 1, j0] + c, p0[i0 - 1, j0 - 1] + c, p0[i0-1, j0 - 2] + c)
-    # feature_extraction.plot_spectrogram(p0[1:, 2:],name="p")
-    # print(p0[-1])
+    feature_extraction.plot_spectrogram(p0[1:, 2:],name="p")
     return p0[-1, -1]
 
 
-# print(dtw(feature_extraction.extract_feature("three2.wav"), feature_extraction.extract_feature("three.wav")))
+print(dtw(feature_extraction.extract_feature("three2.wav"), feature_extraction.extract_feature("three.wav")))
+
+
+# templates is a list of templates
+# def dtw_sychronous(input, templates):
+#     # initialize the padded matrices
+#     n = len(input)
+#     p = [np.full((n+1, len(template)+2), np.inf) for template in templates]
+#     for i in range(n):
+#         # initialize (0,0)
+#         if i == 0:
+#             p0[1,2] = 0
+#             continue
+#         # filter out the nodes we don't go through
+#         for j in range(max(0, m - 2*(n-i-1)-1), min(m, 2*i+1)):
+#             # transform to padded coordinates
+#             i0 = i + 1
+#             j0 = j + 2
+#             c = euclidean_distances(input[i], template[j])
+#             p0[i0, j0] = min(p0[i0 - 1, j0] + c, p0[i0 - 1, j0 - 1] + c, p0[i0-1, j0 - 2] + c)
